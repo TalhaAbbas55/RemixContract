eddb69688197ee2fb8b1cd6ac8d40a9848047ef9
TalhaAbbas55 <talhaabbas556989@gmail.com>
2025-04-24 15:31:53 +0500

    fix contract

--- contracts/SubdomainRegistrar.sol
+++ contracts/SubdomainRegistrar.sol
@@ -85,13 +85,6 @@ contract SubdomainRegistrar1 {
         ensRegistrar.setApprovalForAll(address(nameWrapper), true);
     }
 
-    function onERC721Received(
-    address,
-    address,
-    uint256,
-    bytes calldata
-    ) external pure returns (bytes4) {
-        return this.onERC721Received.selector;
-    }
+    
 
 }
